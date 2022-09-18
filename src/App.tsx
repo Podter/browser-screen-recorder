bbab70d44d032e7c3903d0a3b28123b72e96e40f
Podter <48936015+Podter@users.noreply.github.com>
2022-09-18 14:41:08 +0700

    basic page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,42 @@
 export default function App() {
   return (
     <div>
-      <h1 className="text-center text-2xl font-semibold mt-2">
+      <h1 className="underline decoration-[#486581] decoration-[3px] underline-offset-[6px] text-[220%] font-semibold text-white text-center">
         Screen Recorder
       </h1>
+      <div className="text-center">
+        <video
+          autoPlay={true}
+          controls={false}
+          className="hidden"
+          id="videoFeedback"
+          src=""
+        />
+        <video controls={true} className="hidden" id="videoResult" src="" />
+      </div>
+      <div className="text-center">
+        <button
+          id="downloadBtn"
+          className="bg-[#658148] hover:bg-[#485c33] focus:bg-[#485c33]"
+          onClick={() => console.log("dl btn clicked")}
+        >
+          Download this video ⤵️
+        </button>
+        <button
+          id="startBtn"
+          className="bg-[#486581] hover:bg-[#33485c] focus:bg-[#33485c]"
+          onClick={() => console.log("start rec btn clicked")}
+        >
+          Start recording 🔴
+        </button>
+        <button
+          id="stopBtn"
+          className="hidden bg-[#5c3348] hover:bg-[#5c3348] focus:bg-[#5c3348]"
+          onClick={() => console.log("stop rec btn clicked")}
+        >
+          Stop recording ⬜
+        </button>
+      </div>
     </div>
   );
 }
