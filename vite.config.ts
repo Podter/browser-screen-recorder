1305a18a609604e55891b28625b70084c067de52
Podter <48936015+Podter@users.noreply.github.com>
2022-09-18 14:00:57 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  base: "/browser-screen-recorder/", // remove this if you build and host it local
+  plugins: [react()],
+});
