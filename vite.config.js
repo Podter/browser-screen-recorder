230e13c815a9e702da674af7aef8f0620ea18c8a
Podter <48936015+Podter@users.noreply.github.com>
2022-09-18 20:53:51 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from "vite";
+import crossOriginIsolation from "vite-plugin-cross-origin-isolation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/browser-screen-recorder/",
+  server: {
+    headers: {
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Opener-Policy": "same-origin",
+    },
+  },
+  plugins: [crossOriginIsolation()],
 });
