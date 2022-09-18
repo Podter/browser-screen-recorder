c708551cb7b73e6b1bf0b0cde0e7c36c0a0addfd
Podter <48936015+Podter@users.noreply.github.com>
2022-09-18 21:11:47 +0700

    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import crossOriginIsolation from "vite-plugin-cross-origin-isolation";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +11,25 @@ export default defineConfig({
       "Cross-Origin-Opener-Policy": "same-origin",
     },
   },
-  plugins: [crossOriginIsolation()],
+  plugins: [
+    crossOriginIsolation(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Screen Recorder",
+        short_name: "browser-screen-recorder",
+        description: "A Web Screen Recorder.",
+        theme_color: "#102a43",
+        background_color: "#102a43",
+        icons: [
+          {
+            src: "javascript.svg",
+            sizes: "144x144",
+            type: "image/svg+xml",
+          },
+        ],
+        lang: "en",
+      },
+    }),
+  ],
 });
